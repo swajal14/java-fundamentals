18d6af3aeef93123caab60743e5667a3a56b8498
swajal14 <swajalkumarkachhi@gmail.com>
2026-04-10 15:46:36 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ src/com/java/fundamentals/LoopTest.java
@@ -0,0 +1,9 @@
+package com.java.fundamentals;
+
+public class LoopTest {
+    static void main(String[] args) {
+        for(int i = 1 ;i <= 10 ; System.out.println(++i)){
+            i += 3;
+        }
+    }
+}
